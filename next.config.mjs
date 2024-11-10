d8599d7a6b02e2379bdfccec6eb3340f51f12cb8
ash-codiology <ashfachohan7@gmail.com>
2024-11-10 11:18:09 +0500

    Normalize line endings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['th.bing.com', 'www.bing.com', 'img.freepik.com'], // Add the domains of your external images here
+      },
+      
+};
 
 export default nextConfig;
